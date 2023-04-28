2f989a12892ced11d9d22f77c826d25497acb93f
VadimK <yaralavadim@gmail.com>
2023-04-28 15:59:08 +0300

    refactor

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,33 +1,39 @@
 #!/usr/bin/env python3
 import random
 import prompt
-from brain_games.scripts.brain_games_start import main
+from brain_games.cli import welcome_user
 
-# приветствуем юзера и сохраняем имя
-name = main()
-print('Give three correct answers in a row to win.')
 
-for i in range(3):
-    # формируем случайное число и верный ответ
-    random_num = random.randint(1, 100)
-    correct_answer = 'yes' if random_num % 2 == 0 else 'no'
+def main():
+    # приветствуем юзера и сохраняем имя
+    print('Welcome to the Brain Games!')
+    name = welcome_user()
+    print('Give three correct answers in a row to win.')
 
-    # задаем вопрос
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    print(f'Question: {random_num}')
+    for i in range(3):
+        # формируем случайное число и верный ответ
+        random_num = random.randint(1, 100)
+        correct_answer = 'yes' if random_num % 2 == 0 else 'no'
 
-    # принимаем и проверяем ответ
-    user_answer = prompt.string('Your answer: ')
+        # задаем вопрос
+        print('Answer "yes" if the number is even, otherwise answer "no".')
+        print(f'Question: {random_num}')
 
-    if user_answer == correct_answer:
-        print('Correct!')
+        # принимаем и проверяем ответ
+        user_answer = prompt.string('Your answer: ')
+
+        if user_answer == correct_answer:
+            print('Correct!')
+        else:
+            break
+
+    if i == 2:
+        print(f"Congratulations, {name}!")
     else:
-        break
+        print(f"'{user_answer}' is wrong answer ;(")
+        print(f"Correct answer was '{correct_answer}'")
+        print(f"Let's try again, {name}!")
 
 
-if i == 2:
-    print(f"Congratulations, {name}!")
-else:
-    print(f"'{user_answer}' is wrong answer ;(")
-    print(f"Correct answer was '{correct_answer}'")
-    print(f"Let's try again, {name}!")
+if __name__ == '__main__':
+    main()
