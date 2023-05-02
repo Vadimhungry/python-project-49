4991a23300334db6876bb35dcfd53c54b412f7f8
VadimK <yaralavadim@gmail.com>
2023-05-02 15:27:25 +0300

    add logic.py and refactor

---
+++ brain_games/logic.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from brain_games.cli import welcome_user
+import prompt
+
+def logic(game):
+    # приветствуем юзера и сохраняем имя
+    name = welcome_user()
+
+    for i in range(3):
+        # задаем вопрос и сохраняем ответ
+        answer = game()
+
+        # принимаем ответ
+        user_answer = prompt.string('Your answer: ')
+
+        if user_answer == answer:
+            print('Correct!')
+        else:
+            break
+
+    if i == 2:
+        print(f"Congratulations, {name}!")
+    else:
+        print(f"'{user_answer}' is wrong answer ;(")
+        print(f"Correct answer was '{answer}'")
+        print(f"Let's try again, {name}!")
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,38 +1,10 @@
 #!/usr/bin/env python3
-import random
-import prompt
-from brain_games.cli import welcome_user
-
+from brain_games.games.even import game
+from brain_games.logic import logic
 
 def main():
-    # приветствуем юзера и сохраняем имя
-    print('Welcome to the Brain Games!')
-    name = welcome_user()
-    print('Give three correct answers in a row to win.')
-
-    for i in range(3):
-        # формируем случайное число и верный ответ
-        random_num = random.randint(1, 100)
-        correct_answer = 'yes' if random_num % 2 == 0 else 'no'
-
-        # задаем вопрос
-        print('Answer "yes" if the number is even, otherwise answer "no".')
-        print(f'Question: {random_num}')
-
-        # принимаем и проверяем ответ
-        user_answer = prompt.string('Your answer: ')
-
-        if user_answer == correct_answer:
-            print('Correct!')
-        else:
-            break
-
-    if i == 2:
-        print(f"Congratulations, {name}!")
-    else:
-        print(f"'{user_answer}' is wrong answer ;(")
-        print(f"Correct answer was '{correct_answer}'")
-        print(f"Let's try again, {name}!")
+    # запускаем игру
+    logic(game)
 
 
 if __name__ == '__main__':
