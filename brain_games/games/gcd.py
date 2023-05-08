e3fc6d60785254eeee90e2cdcc35582f2687eb4d
VadimK <yaralavadim@gmail.com>
2023-05-08 12:31:01 +0300

    CodeClimate corrections

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -3,10 +3,8 @@ import random
 
 def find_gcd(a, b):
     # находим наименьший общий делитель
-    if a > b:
-        temp = b
-    else:
-        temp = a
+    temp = b if a > b else a
+
     for i in range(1, temp + 1):
         if ((a % i == 0) and (b % i == 0)):
             gcd = i
