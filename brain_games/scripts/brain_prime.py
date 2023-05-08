f62c9e098480f9193f22392a18b75b2fa6215921
VadimK <yaralavadim@gmail.com>
2023-05-08 12:13:28 +0300

    lint corrections

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from brain_games.games.prime import init_game
 from brain_games.logic import play
-hi1 = 'Answer "yes" if given number is prime.'
+hi1 = 'Answer "yes" if given number is prime. '
 hi2 = 'Otherwise answer "no".'
 welcome_statement = hi1 + hi2
 
