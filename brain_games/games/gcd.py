a69edb291339fd32611699a5151616e4cbe3ff12
VadimK <yaralavadim@gmail.com>
2023-05-09 16:18:38 +0300

    brain_gcd for->while

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -6,12 +6,12 @@ QUERY = 'Find the greatest common divisor of given numbers.'
 def find_gcd(a, b):
     # находим наименьший общий делитель
     temp = b if a > b else a
-
-    for i in range(1, temp + 1):
+    i = 1
+    while i < temp:
         if ((a % i == 0) and (b % i == 0)):
             gcd = i
+        i += 1
     return gcd
-
 
 def init_game():
     a = random.randint(1, 100)
