5ba09020754b8f6c2526b35c45113ed706eb0b7a
VadimK <yaralavadim@gmail.com>
2023-05-09 16:43:51 +0300

    prime.py is_prime() simplify

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,14 +4,12 @@ QUERY = 'Answer "yes" if given number is prime.' \
 
 
 def is_prime(num):
-    k = 0
+
     for i in range(2, num // 2 + 1):
         if (num % i == 0):
-            k = k + 1
-    if (k <= 0):
-        return True
+            return False
     else:
-        return False
+        return True
 
 
 def init_game():
